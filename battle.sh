343ae7838c3cff72bd32b75b82376eea76bbafca
Craig <craig.crj@gmail.com>
2014-05-08 11:40:46 -0600

    Observations.

---
+++ battle.sh
@@ -0,0 +1,4 @@
+python bzagents/pfield_agent.py fishing.cs.byu.edu 6100 &
+#~ python bzagents/craig_dumb_agent.py fishing.cs.byu.edu 5800 &
+#~ python bzagents/craig_dumb_agent.py fishing.cs.byu.edu 5900 &
+#~ python bzagents/craig_dumb_agent.py fishing.cs.byu.edu 6000 &
