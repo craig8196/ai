a4df35ebf5ce8666e7ffafe82ef0bdb2756ad0f4
V <craig.crj@gmail.com>
2014-05-29 14:29:54 -0600

    Added some framework for this lab.

---
+++ runkalman.sh
@@ -0,0 +1,2 @@
+./bin/bzrflag --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 --default-posnoise=5 \
+--respawn-time=10 --seed=0 --world="maps/empty.bzw" --world-size=400
